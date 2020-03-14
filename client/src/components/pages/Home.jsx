32c1cd664644acf2c0ca32d1ddcaf25937adde41
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-03-14 16:14:48 -0700

    added client side

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
 
- const Home = () => {
+const Home = () => {
   return (
-    <div>
-      <h1>
+    <div className="grid-2">
+      <div> <ContactForm /> </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+      {/* <h1>
         home
         <p className="my-1">
         </p>
-      </h1>
+      </h1> */}
     </div>
   )
 }
